b3704cf2ad68d8d17632528385e20bfaea3049b2
Ablavatskii Oleg <37673007+OlegAProject@users.noreply.github.com>
2018-12-22 22:23:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ int k = 0;
 #define ADC1_NUM_CHANNELS   1 
 #define ADC1_BUF_DEPTH      1 
 
-static adcsample_t samples1[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH]; 
+static adcsample_t adc_buffer[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH]; 
+
 
 static const GPTConfig gpt4cfg1 = { 
   .frequency =  100000,
@@ -18,19 +19,15 @@ static const GPTConfig gpt4cfg1 = {
   /* .dier field is direct setup of register, we don`t need to set anything here until now */
 };
 
+
 static void adccallback(ADCDriver *adcp, adcsample_t *buffer, size_t n)
 {
 	adcp = adcp; n = n;
-	if (k < 5000)
-	{
-		k++;
-	}
-	else
-	{
-		k = 0;
-	}
-	pwmEnableChannel(&PWMD3, 2 , k);
+
+    k = buffer[0];
+    chprintf( (BaseSequentialStream *)&SD7, " %d \n\r", buffer[0], n );
 }
+
 
 static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
 
@@ -43,22 +40,23 @@ static const ADCConversionGroup adcgrpcfg1 = {
   /* Buffer will continue writing to the beginning when it come to the end */
   .num_channels = ADC1_NUM_CHANNELS,       
   .end_cb       = adccallback,              
-  .error_cb     = adcerrorcallback,         
+  .error_cb     = adcerrorcallback,        
   .cr1          = 0,
   .cr2          = ADC_CR2_EXTEN_RISING | ADC_CR2_EXTSEL_SRC(12),  // Commutated from GPT
   /* 12 means 0b1100, and from RM (p.449) it is GPT4 */
   /* ADC_CR2_EXTEN_RISING - means to react on the rising signal (front) */
   .smpr1        = ADC_SMPR1_SMP_AN10(ADC_SAMPLE_144),       // for AN10 - 144 samples
-  .smpr2        = 0,        
+  .smpr2        = 0, //ADC_SMPR2_SMP_AN3(ADC_SAMPLE_144),        // for AN3  - 144 samples
   .sqr1         = ADC_SQR1_NUM_CH(ADC1_NUM_CHANNELS),   //
   /* Usually this field is set to 0 as config already know the number of channels (.num_channels) */
   .sqr2         = 0,
-  .sqr3         = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN10)        // sequence of channels
+  .sqr3         = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN10)// |         // sequence of channels
+                  //ADC_SQR3_SQ2_N(ADC_CHANNEL_IN10)
   /* If we can macro ADC_SQR2_SQ... we need to write to .sqr2 */
 };
 
 static const SerialConfig sdcfg = {
-    .speed  = 9600,
+    .speed  = 115200,
     .cr1    = 0,
     .cr2    = 0,
     .cr3    = 0
@@ -73,18 +71,18 @@ void sd_set(void)
 
 void matlab_msg(void)
 {
-	uint16_t two_bytes_number = k;
+	uint32_t array[1] = {k};
 	msg_t msg = sdGetTimeout( &SD7, MS2ST( 30 ) );
 	if ( msg >= 0 )
 		{
-			sdWrite( &SD7, (uint8_t *)&two_bytes_number, sizeof( two_bytes_number ));
+			sdWrite( &SD7, (uint8_t *)array, sizeof( array ));
 		}
 }
 
 void adc_set_and_start(void)
 {
     adcStart(&ADCD1, NULL);
-    adcStartConversion(&ADCD1, &adcgrpcfg1, samples1, ADC1_BUF_DEPTH);
+    adcStartConversion(&ADCD1, &adcgrpcfg1, adc_buffer, ADC1_BUF_DEPTH);
     palSetLineMode( LINE_ADC123_IN10, PAL_MODE_INPUT_ANALOG );  // PC0
 
     gptStart(&GPTD4, &gpt4cfg1);
@@ -92,8 +90,8 @@ void adc_set_and_start(void)
 }
 
 PWMConfig pwm1conf = {
-    .frequency = 500000,
-    .period    = 5000,
+    .frequency = 400000,
+    .period    = 4000,
     .callback  = NULL,
     .channels  = {
                   {PWM_OUTPUT_DISABLED, NULL},
@@ -122,6 +120,8 @@ int main(void)
     while (true)
     {
     	matlab_msg();
-    	chThdSleepMilliseconds(10);
+    	pwmEnableChannel(&PWMD3, 2 , k);
+    	chThdSleepMilliseconds(5);
     }
 }
+
