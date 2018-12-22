08da51227e90ad815d56afcb9a89f20966db7d57
Ablavatskii Oleg <37673007+OlegAProject@users.noreply.github.com>
2018-12-22 22:39:57 +0300

    Update get_number.m

--- get_number.m
+++ get_number.m
@@ -4,11 +4,10 @@ dat.InputBufferSize = 4096;
 
 fopen(dat)
 set(dat, 'ByteOrder', 'littleEndian')
-i = 0;
 disp 'Ok!';
 while 1
-fwrite(dat, 2, 'uint8')
-number = fread(dat, [1,1], 'uint32')
+fwrite(dat, 1, 'uint8')
+number = fread(dat, 1, 'uint16')
 pause(0.1);
 clc;
 end
