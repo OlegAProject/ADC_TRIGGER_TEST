8ea2e1be3baf76b6089be6c9a91ba1da1bc683e7
Ablavatskii Oleg <37673007+OlegAProject@users.noreply.github.com>
2018-12-22 22:25:44 +0300

    Update get_number.m

--- get_number.m
+++ get_number.m
@@ -1,15 +1,15 @@
 delete(instrfind);
-dat = serial('COM7', 'BaudRate', 9600);
+dat = serial('COM7', 'BaudRate', 115200);
 dat.InputBufferSize = 4096;
 
 fopen(dat)
 set(dat, 'ByteOrder', 'littleEndian')
-
+i = 0;
 disp 'Ok!';
 while 1
-fwrite(dat, 1, 'uint8')
-number = fread(dat, 1, 'uint16')
-pause(0.05);
+fwrite(dat, 2, 'uint8')
+number = fread(dat, [1,1], 'uint32')
+pause(0.1);
 clc;
 end
 fclose(dat);
